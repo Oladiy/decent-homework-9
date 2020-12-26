646ebb38318e39728e54f78d9ec1fed231a9a377
Oladiy <bd3396121@gmail.com>
2020-12-27 02:09:37 +0300

    Initial commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const CommitRevealAuction = artifacts.require("CommitRevealAuction");
 
 module.exports = function (deployer) {
-    deployer.deploy(CommitRevealAuction);
+    deployer.deploy(CommitRevealAuction, 10, 20, "0xC9c4dB81f75643FAeb5e38CF3a303578bfe6788b", "Vase");
 };
